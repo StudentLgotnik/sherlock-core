51c85365fc5f047e10a3cc1d62e4553825377106
Maksym Kazarin <kazzarin99@gmail.com>
2021-09-19 21:42:23 +0300

    Circleci init and deploy plugin was added

--- src/test/java/com/mkaza/sherlock/api/TestCaseSherlockIntegrationTest.java
+++ src/test/java/com/mkaza/sherlock/api/TestCaseSherlockIntegrationTest.java
@@ -9,6 +9,10 @@ import org.junit.Test;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 
 public class TestCaseSherlockIntegrationTest {
@@ -35,5 +39,40 @@ public class TestCaseSherlockIntegrationTest {
         Assert.assertEquals(2, clusters.size());
         Assert.assertEquals(6, clusters.get(0).getCases().size());
         Assert.assertEquals(5, clusters.get(1).getCases().size());
+    }
+
+    @Test
+    public void concurrencyTest() throws InterruptedException, ExecutionException {
+        ExecutorService executor
+                = Executors.newFixedThreadPool(2);
+
+        SherlockConfig config = SherlockConfig.builder(testLogsPath).parser(new MockParser()).build();
+
+        Sherlock<TestCaseCluster> sherlock = new TestCaseSherlock(config);
+
+        SherlockConfig config2 = SherlockConfig.builder("D:\\Labs\\Masters_diploma\\project\\SherlockLogsProvider\\target\\surefire-reports\\TEST-com.mkaza.sherlock.api.UserApiTest.xml").build();
+
+        Sherlock<TestCaseCluster> sherlock2 = new TestCaseSherlock(config2);
+
+        Future<List<TestCaseCluster>> future1 = executor.submit(sherlock::cluster);
+
+        Future<List<TestCaseCluster>> future2 = executor.submit(sherlock2::cluster);
+
+        while(!future1.isDone() && !future2.isDone()) {
+            System.out.println("Calculating...");
+            Thread.sleep(300);
+        }
+
+        List<TestCaseCluster> result1 = future1.get();
+        List<TestCaseCluster> result12 = future2.get();
+    }
+
+    public Future<Integer> calculate(Integer input) {
+        ExecutorService executor
+                = Executors.newSingleThreadExecutor();
+        return executor.submit(() -> {
+            Thread.sleep(1000);
+            return input * input;
+        });
     }
 }
